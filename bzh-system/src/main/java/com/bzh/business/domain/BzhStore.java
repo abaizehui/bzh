9fab7ad806d903aa26e055b68807d665016986b5
abaizehui <15811456288@163.com>
2025-03-02 19:22:26 +0800

    add

--- bzh-system/src/main/java/com/bzh/business/domain/BzhStore.java
+++ bzh-system/src/main/java/com/bzh/business/domain/BzhStore.java
@@ -49,6 +49,10 @@ public class BzhStore extends BaseEntity
     private String businessHours;
     @Excel(name = "小程序id")
     private String appId;
+    @Excel(name = "企业微信ID")
+    private String corpId;
+    @Excel(name = "客服链接")
+    private String extInfo;
     /** 状态（0正常 1停用） */
     @Excel(name = "状态", readConverterExp = "0=正常,1=停用")
     private String status;
